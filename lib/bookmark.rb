fadc25aeb3884aaa69a097daa2f70eda727ed5b2
Kay <kard@hotmail.co.uk>
2022-06-09 17:19:10 +0100

    refactored .create and .title methods

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -9,10 +9,12 @@ class Bookmark
 
   def self.create(url, title)
     controller = PG.connect :dbname => ENV['BOOKMARK_DATABASE']
-    controller.exec "INSERT INTO bookmarks(url) VALUES('#{url}');"
+    controller.exec "INSERT INTO bookmarks(url, title) VALUES('#{url}', '#{title}');"
   end
 
   def self.title
-    ['Makers Academy', 'Destroy All Software', 'Google']
+    controller = PG.connect :dbname => ENV['BOOKMARK_DATABASE']
+    rs = controller.exec "SELECT * FROM bookmarks;"
+    rs.map { |row| row['title'] }
   end
 end

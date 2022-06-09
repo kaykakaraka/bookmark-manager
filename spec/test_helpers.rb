6a6e61172f5b88017247bd1b7d6f971f123871db
Kay <kard@hotmail.co.uk>
2022-06-09 12:47:15 +0100

    refactored test helpers to use .create method

--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -3,14 +3,11 @@ def truncate_table
   controller.exec('TRUNCATE TABLE bookmarks;')
 end
 def add_makers_url
-  controller = PG.connect :dbname => ENV['BOOKMARK_DATABASE']
-  controller.exec("INSERT INTO bookmarks(url) VALUES('http://www.makersacademy.com/');")
+  Bookmark.create('http://www.makersacademy.com/')
 end
 def add_destroyallsoftware_url
-  controller = PG.connect :dbname => ENV['BOOKMARK_DATABASE']
-  controller.exec("INSERT INTO bookmarks(url) VALUES('http://www.destroyallsoftware.com');")
+  Bookmark.create('http://www.destroyallsoftware.com')
 end
 def add_google_url
-  controller = PG.connect :dbname => ENV['BOOKMARK_DATABASE']
-  controller.exec("INSERT INTO bookmarks(url) VALUES('http://www.google.com/');")
+  Bookmark.create('http://www.google.com/')
 end
